d9d5b09663342e5350386d01d8380424f19fed38
Lakshmi Prasad <119niki@gmail.com>
2016-07-01 11:06:25 +0530

    changed JDK

--- jenkinstest/src/test/java/com/sample/jenkinstest/Sample.java
+++ jenkinstest/src/test/java/com/sample/jenkinstest/Sample.java
@@ -1,13 +1,35 @@
 package com.sample.jenkinstest;
 
-import org.testng.annotations.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class Sample {
-	@Parameters({"propertyName"})
+	WebDriver driver = null;
 	@Test
-	public void f(String browser) {
+	public void f() {
 		String value = System.getProperty("propertyName");
-		System.out.println(value);
+		
+		if(value.equalsIgnoreCase("firefox")){
+			driver = new FirefoxDriver();
+		}
+		else if(value.equalsIgnoreCase("chrome")){
+			driver = new ChromeDriver();
+		}
+		else if(value.equalsIgnoreCase("ie")){
+			driver = new InternetExplorerDriver();
+		}
+		
+		driver.get("http://www.google.com");
+		try{
+			Thread.sleep(3000);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		driver.close();
+		driver.quit();
 	}
 }
